aeaf0671d0952495fbbdf5cd302c9466007ccb97
Aditi <aditisriva21@gmail.com>
2025-11-30 01:02:31 +0530

    Fix: cleaned .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './component/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './component/Footer/Footer';
+import LoginPopup from './component/LoginPopup/LoginPopup';
+import { useState } from 'react';
+
+function App() {
+ const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <>
+      {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <Navbar setShowLogin={setShowLogin} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
